657533843b2e7008d2fc44274eabd150c9dbbab5
Gerlielbecario <fernando.huaranca99@gmail.com>
2024-06-21 21:31:06 -0300

    agrego definicion de metrica

--- work/2-Datos_Sudamerica/analisis/funcion_ets.py
+++ work/2-Datos_Sudamerica/analisis/funcion_ets.py
@@ -1,5 +1,20 @@
+#Este codigo realiza el calculo de la metrica 
+#Equitable threat Score
+
+#https://confluence.ecmwf.int/display/FUG/Section+6.2.3+Equitable+Threat+Score
+
+#En muy pocas palabras permite evaluar la confibialidad de un pronostico en 
+#un area. Lo especial que tiene esta metrica es que teoricamente nos permite
+#desprendernos de el factor espacial.
+#Yo podria comparar el ETS de un pais, con el de otro pais distante, y en 
+#resumidas cuentas este valor me seguiria siendo util.
+
+
 import numpy as np
 import os
+
+
+
 
 def ETS(Umbrales,Matriz_stack):
 
